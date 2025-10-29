62896537f94a141add46fed7b882c9c142c62952
Ninna Melo <ninna.devsystem@gmail.com>
2025-10-29 14:58:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
+// Menu mobile
 const hamburger = document.getElementById('hamburger');
 const menu = document.querySelector('.menu');
-
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('active');
+});
+
+// Modal interativo
+const modal = document.getElementById('modal');
+const openModal = document.getElementById('openModal');
+const closeModal = document.getElementById('closeModal');
+
+openModal.addEventListener('click', () => {
+  modal.style.display = 'flex';
+});
+
+closeModal.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) modal.style.display = 'none';
 });
